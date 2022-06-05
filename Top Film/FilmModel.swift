b5a165e315d23b56e27f4ea9935c3d8b34483aa6
Oleksandr Oselskyi <macoselsky@gmail.com>
2022-06-05 23:44:41 +0300

    Initial

---
+++ Top Film/FilmModel.swift
@@ -0,0 +1,26 @@
+//
+//  FilmModel.swift
+//  Top Film
+//
+//  Created by Maks on 05.06.2022.
+//
+
+import Foundation
+
+//class Film {
+//    @ objc dynamic var image: String?
+//    @ objc dynamic var title: String
+//    @ objc dynamic var rate: String?
+//    @ objc dynamic var overview: String
+//    @ objc dynamic var date: String?
+//
+//
+//convenience init(image: String?, title: String, rate: String?, overview: String, date: String?) {
+//    self.init
+//    self.image = image
+//    self.title = title
+//    self.rate = rate
+//    self.overview = overview
+//    self.date = date
+//    }
+//}
--- Top Film/ViewController.swift
+++ Top Film/ViewController.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let filmTitle = ["Ремінісценція",
+                     "Перехоплювач",
+                     "Загублене місто"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filmTitle.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = filmTitle[indexPath.row]
+        cell.imageView?.image = UIImage(named: filmTitle[indexPath.row])
+        return cell
+    }
 }
 
